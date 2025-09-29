315224b83a6d6da3ede0778b96a19f0fb3096451
MohammedAmr04 <mohammedamr200497@gmail.com>
2025-09-29 19:24:55 +0300

    feat: add VideoSection component and integrate it into HomePage

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,7 +1,9 @@
+import { Video } from "iconsax-react";
 import BannarSection from "../components/home/bannarSection/BannarSection";
 import FeaturedSection from "../components/home/featuredSection/FeaturedSection";
 import PopularCategories from "../components/home/popularCategories/PopularCategories";
 import PopularProducts from "../components/home/popularProducts/PopularProducts";
+import VideoSection from "../components/home/videoSection/VideoSection";
 
 function HomePage() {
   return (
@@ -10,6 +12,7 @@ function HomePage() {
       <FeaturedSection />
       <PopularCategories />
       <PopularProducts />
+      <VideoSection />
     </div>
   );
 }
