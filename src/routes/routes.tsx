4923b3eb9effc51db5337df427cde75f8bd33ad6
Omar Ahmed <omar.ahmed9806@gmail.com>
2025-10-05 23:08:55 +0300

    fixed the routes

--- src/routes/routes.tsx
+++ src/routes/routes.tsx
@@ -2,7 +2,7 @@ import { createBrowserRouter } from "react-router";
 import Error404 from "../components/error/Error404";
 import { MainLayout } from "../components";
 import HomePage from "../pages/HomePage";
-import SignUp from "../components/auth/SignUp";
+import SignUp from "../components/auth/SignUpForm";
 import Login from "../components/auth/LoginForm";
 import Contact from "../pages/ContactPage";
 
@@ -10,7 +10,7 @@ import Contact from "../pages/ContactPage";
 const routes = createBrowserRouter([
   {
     path: "/",
-    element: <Error404/>,
+    errorElement: <Error404/>,
     element: <MainLayout />,
     children: [
       { index: true, element: <HomePage /> },
